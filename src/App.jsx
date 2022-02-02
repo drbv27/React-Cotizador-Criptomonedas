af4580f93f8574a8f62e85a097c9ad6bf69bd17b
Diego <drbv27@gmail.com>
2022-02-02 07:12:35 -0500

    first styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
+import styled from "@emotion/styled";
+
+const Heading = styled.h1`
+  font-family: "Lato", sans-serif;
+  color: #fff;
+`;
 
 function App() {
-  return <h1>Desde App</h1>;
+  return <Heading>Desde App</Heading>;
 }
 
 export default App;
