1a88740aec1d116d1b6527b76d6cf87556c09b98
Diego <drbv27@gmail.com>
2022-02-04 12:11:49 -0500

    formating the res

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,26 +1,58 @@
 import styled from "@emotion/styled";
+
+const ContenedorRes = styled.div`
+  color: #fff;
+  font-family: "Lato", sans-serif;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 2rem;
+`;
+const Imagen = styled.img`
+  display: block;
+  width: 25%;
+`;
+const Texto = styled.p`
+  font-size: 1.1rem;
+  span {
+    font-weight: 700;
+  }
+`;
+const Precio = styled.p`
+  font-size: 1.5rem;
+  span {
+    font-weight: 700;
+  }
+`;
+
 const Resultado = ({ resultado }) => {
   const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
     resultado;
   console.log(resultado);
   return (
-    <div>
-      <p>
-        El Precio es de: <span>{PRICE}</span>
-      </p>
-      <p>
-        Precio más alto del día: <span>{HIGHDAY}</span>
-      </p>
-      <p>
-        Precio más bajo del día: <span>{LOWDAY}</span>
-      </p>
-      <p>
-        Porcentaje de variación 24H: <span>{CHANGEPCT24HOUR}</span>
-      </p>
-      <p>
-        Ultima actualización: <span>{LASTUPDATE}</span>
-      </p>
-    </div>
+    <ContenedorRes>
+      <Imagen
+        src={`https://cryptocompare.com/${IMAGEURL}`}
+        alt="imagencripto"
+      />
+      <div>
+        <Precio>
+          El Precio es de: <span>{PRICE}</span>
+        </Precio>
+        <Texto>
+          Precio más alto del día: <span>{HIGHDAY}</span>
+        </Texto>
+        <Texto>
+          Precio más bajo del día: <span>{LOWDAY}</span>
+        </Texto>
+        <Texto>
+          Porcentaje de variación 24H: <span>{CHANGEPCT24HOUR}</span>
+        </Texto>
+        <Texto>
+          Ultima actualización: <span>{LASTUPDATE}</span>
+        </Texto>
+      </div>
+    </ContenedorRes>
   );
 };
 
